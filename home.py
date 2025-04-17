1508826022b56e824b1a9061480938497b084f8d
Big165 <Seagame.w2005@gmail.com>
2025-04-17 13:12:17 +0700

    .

--- home.py
+++ home.py
@@ -35,3 +35,48 @@ st.write(dt.head(10))
 st.subheader("ข้อมูลส่วนแรก 10 แถว")
 dt = pd.read_csv("./data/diabetes-dataset.csv")
 st.write(dt.head(10))
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
+pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
+pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
+
+sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
+sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
+
+A1 = st.number_input("ข้อมูล 1")
+A2 = st.number_input("ข้อมูล 2")
+A3 = st.number_input("ข้อมูล 3")  
+A4 = st.number_input("ข้อมูล 4")
+A5 = st.number_input("ข้อมูล 5")
+A6 = st.number_input("ข้อมูล 6")
+A7 = st.number_input("ข้อมูล 7")
+A8 = st.number_input("ข้อมูล 8")
+A9 = st.number_input("ข้อมูล 9")
+A10 = st.number_input("ข้อมูล 10")
+A11 = st.number_input("ข้อมูล 11")
+
+
+
+if st.button("ทำนายผล"):
+    #st.write("ทำนาย")
+   dt = pd.read_csv("./data/heart.csv") 
+   X = dt.drop('HeartDisease', axis=1)
+   y = dt.HeartDisease
+
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)  
+    
+   x_input = np.array([[A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11]])
+   st.write(Knn_model.predict(x_input))
+   
+   out=Knn_model.predict(x_input)
+
+   if out[0] == 1:
+    st.image("./img/one.jpg")
+   elif out[0] == 'Setosa':
+    st.image("./img/two.jpg")
+ 
+else:
+    st.write("ไม่ทำนาย")
