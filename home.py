ae3ec3d619ccb385743102542e1489e2f06975a1
Big165 <Seagame.w2005@gmail.com>
2025-04-18 08:27:47 +0700

    .

--- home.py
+++ home.py
@@ -45,10 +45,33 @@ if st.button("กดตรงนี้เพื่อดูผล"):
    if out[0] == 1:
     st.image('./img/THREE.jpg', width=1000)
     st.markdown("<h2 style='text-align: center; color: orange;'>เสียใจด้วย คุณเป็นโรคเบาหวาน</h2>",unsafe_allow_html=True)
+    st.markdown(
+    """
+    <div style='text-align: center;'>
+        <h2 style='color: #001f3f;'>กรุณาตอบคำถามอย่างครบถ้วน</h2>
+        <p style='color: #666666; font-size: 16px;'>
+            แนะนำให้คุณปรึกษาแพทย์หรือผู้เชี่ยวชาญทางการแพทย์เพื่อรักษา<br>
+            แต่ไม่ต้องกลัวไป ถึงมันอาจจะยากและคุณอาจจะกลัว แต่ไม่ว่าอย่างไรคุณจะต้องผ่านมันไปได้แน่นอน
+        </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
    elif out[0] == 0:
     st.image('./img/TWO.jpg', width=1000)
     st.markdown("<h2 style='text-align: center; color: orange;'>ยินดีด้วย คุณไม่เป็นโรคเบาหวาน</h2>",unsafe_allow_html=True)
- 
+  st.markdown(
+    """
+    <div style='text-align: center;'>
+        <h2 style='color: #001f3f;'>กรุณาตอบคำถามอย่างครบถ้วน</h2>
+        <p style='color: #666666; font-size: 16px;'>
+            เยี่ยมยอมมาก! <br>
+            รักษาสุขภาพของคุณไว้ให้ดีต่อไปนะ ขอให้มีความสุข
+        </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 else:
     st.write("ไม่ทำนาย")
 
